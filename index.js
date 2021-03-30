fa5d509781f4aaa005ee7abf386c83ec57700e8b
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-03-30 12:09:24 -0700

    created index.js file; declared and gave value to 2 variables for file system and inquirer in js file; added content in questions array for the title and the description section of README.file.

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What was your motivation for the project?",
+        name: "motivation",
+    }
+    {
+        type: "input",
+        message: "Why did you build this project?",
+        name: "why",
+    }
+    {
+        type: "input",
+        message: "What problem does it solve?",
+        name: "solve",
+    }
+    {
+        type: "input",
+        message: "What did you learn?",
+        name: "learn",
+    }
+    {
+        type: "input",
+        message: "What makes your project stand out?",
+        name: "standout",
+    }
+    {
+        type: ,
+        message: ,
+        name: ,
+    }
+    {
+        type: ,
+        message: ,
+        name: ,
+    }
+    {
+        type: ,
+        message: ,
+        name: ,
+    }
+    {
+        type: ,
+        message: ,
+        name: ,
+    }
+    {
+        type: ,
+        message: ,
+        name: ,
+    }
+    {
+        type: ,
+        message: ,
+        name: ,
+    }
+    {
+        type: ,
+        message: ,
+        name: ,
+    }
+]
