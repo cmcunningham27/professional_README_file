af3122ee1941c5a86274f47414f7b3deede22243
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-04-05 18:12:55 -0700

    license badge changes in index.js and README.md.

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const questions = [
         type: "list",
         message: "What license do you choose for your project?",
         name: "license",
-        choices: ["Apache-License-2.0", "GNU-General-Public-License-v3.0", "MIT-License", "BSD-2-Clause-License", "BSD-3-Clause-License", "Boost-Software-License-1.0", "Creative-Commons-Zero-v1.0-Universal", "Eclipse-Public-License-2.0", "GNU-Affero-General-Public-License-v3.0", "GNU-General-Public-License-v2.0", "GNU-Lesser-General-Public-License-v2.1", "Mozilla-Public-License-v2.0", "The-Unlicense"],
+        choices: ["Apache%202.0", "GNU%General%Public%License%v3.0", "MIT", "BSD%202", "BSD%203", "Boost%201.0", "CC0%201.0", "EPL%201.0", "GPLv3", "GPL%20v2", "LGPL%20v3", "MPL%202.0", "Unlicense"],
     },
     {
         type: "input",
